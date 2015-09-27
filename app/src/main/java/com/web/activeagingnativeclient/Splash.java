893aa40cddddb2eaafa3fac9c95e9b0b5a2189e8
Christian <christian926@homtail.com>
2015-09-27 13:12:56 +0200

    rensade importen i en klass och lade till en ny konstant

--- app/src/main/java/com/web/activeagingnativeclient/Splash.java
+++ app/src/main/java/com/web/activeagingnativeclient/Splash.java
@@ -7,8 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
